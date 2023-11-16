a61040e44eaff4d7a10e6c02dc4c6e11ce57313f
Amin <medamineharwel@outlook.com>
2023-11-16 16:21:13 +0100

    fixed browser animation module error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserModule, BrowserAnimationsModule),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes)
   ]
