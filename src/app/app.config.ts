be2a30facab66ed5a0ec0497b368fcc4294b89d5
Amin <medamineharwel@outlook.com>
2023-11-21 15:43:31 +0100

    added ngrx and root module store setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
+import { StoreModule } from "@ngrx/store";
+import { metaReducers, reducers } from "./store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(BrowserModule, BrowserAnimationsModule),
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+      StoreModule.forRoot(reducers, {metaReducers}),
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+      !environment.production ? StoreDevtoolsModule.instrument() : [],
+    ),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes)
   ]
---
+++ src/app/store/index.ts
@@ -0,0 +1,18 @@
+import { isDevMode } from '@angular/core';
+import {
+  ActionReducerMap,
+  MetaReducer
+} from '@ngrx/store';
+
+export const stateFeatureKey = 'state';
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+};
+
+
+export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
