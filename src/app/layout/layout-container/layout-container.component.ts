18e0c21eac0720e76078bddec1a2776f7f0a90da
Amin <medamineharwel@outlook.com>
2023-11-21 17:10:03 +0100

    dispatch theme actions in layout comonent

--- src/app/layout/layout-container/layout-container.component.ts
+++ src/app/layout/layout-container/layout-container.component.ts
@@ -3,7 +3,11 @@ import { NavTree } from 'src/app/shared/types/util.interfaces';
 import { SideNavComponent } from '../side-nav/side-nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { DOCUMENT } from '@angular/common';
+import { AsyncPipe, DOCUMENT } from '@angular/common';
+import { Store, select } from '@ngrx/store';
+import {ThemeActions} from 'src/app/store/actions/theme.actions';
+import * as fromTheme from 'src/app/store/selectors/theme.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout-container',
@@ -11,32 +15,37 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./layout-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [SideNavComponent, MatSidenavModule, MatSlideToggleModule]
+  imports: [SideNavComponent, MatSidenavModule, MatSlideToggleModule, AsyncPipe]
 })
 export class LayoutContainerComponent implements OnInit {
   opened: boolean = true;
   selectedTheme = '';
   currentTheme = 'dark-theme';
 
+  darkMode$: Observable<boolean>;;
+
 
   @Input() navTree: NavTree[] = [];
   @ViewChild('drawer') drawer!: ElementRef;
+
+  constructor(public store: Store) {
+    this.darkMode$ = this.store.pipe(select(fromTheme.selectDarkModeState))
+  }
 
   toggleDrawer() {
     this.opened = !this.opened;
   }
 
-  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {}
-
   ngOnInit(): void {
-    this.currentTheme = localStorage.getItem('activeTheme') || 'dark-theme';
-    this.renderer.setAttribute(this.document.body, 'class', this.currentTheme);
+    this.store.dispatch(ThemeActions.initTheme());
   }
 
   switchTheme({ checked }: MatSlideToggleChange) {
-    this.currentTheme = checked ? 'dark-theme' : 'light-theme';
-    this.renderer.setAttribute(this.document.body, 'class', this.currentTheme);
-    localStorage.setItem('activeTheme', this.currentTheme);
+    if (checked) {
+      this.store.dispatch(ThemeActions.enableDarkMode());
+    } else {
+      this.store.dispatch(ThemeActions.disableDarkMode());
+    }
   }
 
 }
